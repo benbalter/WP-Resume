8c665a0cfe1c5ceba9ebc51012e442ef34b48e88
Benjamin J. Balter <ben@balter.com>
2010-09-19 21:32:22 +0000

    Version 1.2 (beta)

--- resume.php
+++ resume.php
@@ -5,50 +5,49 @@
  * @author Benjamin J. Balter
  * @since 1.0a
  */
+define('WP_USE_THEMES', false); 
 get_header();
-$options = get_option('wpr_options');
+$options = wp_resume_get_options();
 ?>
 <div id='container'>
-<div id='content' role="main">
-<div class='resume'>
-<div id='bar'>&nbsp;</div>
-<div id='header'>
-	<h2><?php echo $options['title']; ?></h2>
-	<?php echo $options['contact_info']; ?>
-	</div>
-<?php 
-
-$sections = wpr_get_sections();
-
-foreach ($sections as $section) { ?>
-	<div class="section" id="<?php echo $section->slug; ?>">
-		<div class="label"><?php echo $section->name; ?></div>
-<?php
-		$current_org='';
-		foreach ( wpr_get_blocks( $section->slug ) as $block) {
-			$organization = wp_get_object_terms( $block->ID, 'wpr_organization' );
-			
-			if ($organization[0]->term_id != $current_org) {
-				if ($current_org != '') { ?>
-	</div>
-<?php }	
-				$current_org = $organization[0]->term_id; ?>
-		<div class="organization" id="<?php echo $organization[0]->slug; ?>">
-			<div class="orgName"><?php echo $organization[0]->name; ?></div>
-			<div class="location"><?php echo $organization[0]->description; ?></div>
-<?php 		}  ?>
-			<div class="title"><?php echo $block->post_title; ?></div>
-			<div class="date"><?php echo wpr_format_date($block->ID); ?></div>
-			<div class="details">
-<?php 		echo $block->post_content; ?>
+	<div id='content' role="main">
+		<div class='resume'>
+			<div id='bar'>&nbsp;</div>
+			<div id='header'>
+				<h2><?php echo $options['title']; ?></h2>
+<?php 			echo $options['contact_info']; ?>
 			</div>
-<?php 	}  ?>
+<?php 		foreach ( wp_resume_get_sections() as $section) { ?>
+			<div class="section" id="<?php echo $section->slug; ?>">
+				<div class="label"><?php echo $section->name; ?></div>
+<?php			$current_org='';
+				wp_resume_query( $section->slug );
+				if ( have_posts() ) : while ( have_posts() ) : the_post();
+				$organization = wp_resume_get_org( $post->ID );
+					if ($organization->term_id != $current_org) {
+						if ($current_org != '') { ?>
+				</div>
+<?php 					$current_org = $organization->term_id;
+						}	?>
+				<div class="organization" id="<?php echo $organization->slug; ?>">
+					<div class="orgName"><?php echo $organization->name; ?></div>
+					<div class="location"><?php echo $organization->description; ?></div>
+<?php 				}  ?>
+					<div class="title"><?php echo the_title(); ?></div>
+					<div class="date"><?php echo wp_resume_format_date($post->ID); ?></div>
+					<div class="details">
+						<?php the_content(); ?>
+<?php if ( current_user_can( 'edit_posts' ) ) 
+	edit_post_link('Edit'); ?>
+					</div>
+				</div>
+<?php 	endwhile; endif; ?>
+			</div>
+<?php 
+	}
+?>
 		</div>
 	</div>
-<?php 
-}
-?>
-</div></div>
 <?php 
 if ( $options['sidebar'] ) 
 	get_sidebar(); 
