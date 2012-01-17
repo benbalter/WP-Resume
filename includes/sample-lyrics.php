c4295f0fde7ed57f4e01257a04cc68de7274a83d
Benjamin J. Balter <ben@balter.com>
2012-01-17 13:56:39 -0500

    js fixes

--- includes/sample-lyrics.php
+++ includes/sample-lyrics.php
@@ -8,36 +8,7 @@
 class Hello_Dolly2_Sample_Lyrics {
 	
 	static $parent;
-	public $lyrics = array ( 
-							"Hello, Dolly",
-	    					"Well, hello, Dolly",
-	    					"It's so nice to have you back where you belong",
-	    					"You're lookin' swell, Dolly",
-	    					"I can tell, Dolly",
-	    					"You're still glowin', you're still crowin'",
-	    					"You're still goin' strong",
-	    					"We feel the room swayin'",
-	    					"While the band's playin'",
-	    					"One of your old favourite songs from way back when",
-	    					"So, take her wrap, fellas",
-	    					"Find her an empty lap, fellas",
-	    					"Dolly'll never go away again",
-	    					"Hello, Dolly",
-	    					"Well, hello, Dolly",
-	    					"It's so nice to have you back where you belong",
-	    					"You're lookin' swell, Dolly",
-	    					"I can tell, Dolly",
-	    					"You're still glowin', you're still crowin'",
-	    					"You're still goin' strong",
-	    					"We feel the room swayin'",
-	    					"While the band's playin'",
-	    					"One of your old favourite songs from way back when",
-	    					"Golly, gee, fellas",
-	    					"Find her a vacant knee, fellas",
-	    					"Dolly'll never go away",
-	    					"Dolly'll never go away",
-	    					"Dolly'll never go away again",
-	    			);
+	public $lyrics;
 	
 	function __construct( &$instance ) {
 		
@@ -47,12 +18,50 @@ class Hello_Dolly2_Sample_Lyrics {
 		else
 			self::$parent = &$instance;
 			
+		$this->lyrics = array ( 
+							__( "Hello ), Dolly" ),
+	    					__( "Well ), hello ), Dolly" ),
+	    					__( "It's so nice to have you back where you belong" ),
+	    					__( "You're lookin' swell ), Dolly" ),
+	    					__( "I can tell ), Dolly" ),
+	    					__( "You're still glowin' ), you're still crowin'" ),
+	    					__( "You're still goin' strong" ),
+	    					__( "We feel the room swayin'" ),
+	    					__( "While the band's playin'" ),
+	    					__( "One of your old favourite songs from way back when" ),
+	    					__( "So ), take her wrap ), fellas" ),
+	    					__( "Find her an empty lap ), fellas" ),
+	    					__( "Dolly'll never go away again" ),
+	    					__( "Hello ), Dolly" ),
+	    					__( "Well ), hello ), Dolly" ),
+	    					__( "It's so nice to have you back where you belong" ),
+	    					__( "You're lookin' swell ), Dolly" ),
+	    					__( "I can tell ), Dolly" ),
+	    					__( "You're still glowin' ), you're still crowin'" ),
+	    					__( "You're still goin' strong" ),
+	    					__( "We feel the room swayin'" ),
+	    					__( "While the band's playin'" ),
+	    					__( "One of your old favourite songs from way back when" ),
+	    					__( "Golly ), gee ), fellas" ),
+	    					__( "Find her a vacant knee ), fellas" ),
+	    					__( "Dolly'll never go away" ),
+	    					__( "Dolly'll never go away" ),
+	    					__( "Dolly'll never go away again" ),
+	    			);
+			
 		self::$parent->options->defaults = &$this->lyrics;
 			
 		add_filter( self::$parent->slug . '_lyric', array( &$this, 'add_p_tag' ) );
 		add_action( 'admin_notices', array( &$this, 'lyric' ) );
 		add_action( 'admin_head', array( &$this, 'css' ) );
+		add_action( 'admin_init', array( &$this, 'js_lyrics' ) );
 		
+	}
+	
+	function get_lyrics() {
+		$lyrics = self::$parent->options->lyrics;
+		array_walk( &$lyrics, 'wptexturize' );
+		return $lyrics;
 	}
 
 	/**
@@ -61,10 +70,17 @@ class Hello_Dolly2_Sample_Lyrics {
 	 */
 	function get_lyric() {
 
-		$lyrics = self::$parent->options->lyrics;
-		$lyric = wptexturize( $lyrics[ mt_rand( 0, count( $lyrics ) - 1 ) ] );
+		$lyrics = $this->get_lyrics();
+		$lyric = $lyrics[ mt_rand( 0, count( $lyrics ) - 1 ) ];
 		return self::$parent->api->apply_filters( 'lyric', $lyric );
 		
+	}
+	
+	/**
+	 * Enqueue lyrics via localize script
+	 */
+	function js_lyrics() {
+		self::$parent->enqueue->admin_data['lyrics'] = $this->get_lyrics();
 	}
 	
 	/**
--- js/admin/sample-admin.dev.js
+++ js/admin/sample-admin.dev.js
@@ -1,5 +1,5 @@
-jQuery( document ).ready( function( $ ) ) {
-	$('#dolly').click( function( ) ) {
-	
-	}
-}
+jQuery( document ).ready( function( $ ) {
+	$('#dolly').click( function( ) {
+		$(this).fadeOut( 'slow', function() { $(this).html( hd2.lyrics[ Math.floor(Math.random() * hd2.lyrics.length ) ] ) } ).fadeIn( 'slow' );
+	});
+});
